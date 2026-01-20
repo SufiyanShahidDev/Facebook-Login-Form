4658af84bd653a657f16b65e309f49dbe39613a7
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-20 20:43:45 +0500

    Initial commit

--- login.js
+++ login.js
@@ -1,51 +1,51 @@
 
-let email = document.getElementById("email");
+// let email = document.getElementById("email");
 
-let password = document.getElementById("password");
+// let password = document.getElementById("password");
 
-let loginBtn = document.getElementById("loginBtn");
+// let loginBtn = document.getElementById("loginBtn");
 
-let userDataArr = [];
+// let userDataArr = [];
 
-let loginHandler = () => {
-    const sweetAlert = (error, title, message) => {
-        Swal.fire({
-            icon: error,
-            title: title,
-            text: message,
-        });
-    };
+// let loginHandler = () => {
+//     const sweetAlert = (error, title, message) => {
+//         Swal.fire({
+//             icon: error,
+//             title: title,
+//             text: message,
+//         });
+//     };
 
-    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+//     let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if (!emailRegex.test(email.value.trim())) {
-        sweetAlert("error", "Something Went Wrong", "Please Enter a valid Email Address")
-        return;
-    };
+//     if (!emailRegex.test(email.value.trim())) {
+//         sweetAlert("error", "Something Went Wrong", "Please Enter a valid Email Address")
+//         return;
+//     };
 
-    let passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
+//     let passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
 
-    if (!passwordRegex.test(password.value)) {
-        sweetAlert("error", "Weak Password", "Password must contain uppercase, number & special character");
-        return;
-    };
+//     if (!passwordRegex.test(password.value)) {
+//         sweetAlert("error", "Weak Password", "Password must contain uppercase, number & special character");
+//         return;
+//     };
 
-    let userData = {
-        email: email.value,
-        password: password.value
-    };
+//     let userData = {
+//         email: email.value,
+//         password: password.value
+//     };
 
-    userDataArr.push(userData);
+//     userDataArr.push(userData);
 
-    console.log(userDataArr);
+//     console.log(userDataArr);
 
-    localStorage.setItem("users", JSON.stringify(userDataArr));
+//     localStorage.setItem("users", JSON.stringify(userDataArr));
 
-    sweetAlert("success", "Login Successfully", "Congratulations");
+//     sweetAlert("success", "Login Successfully", "Congratulations");
 
-    email.value = "";
+//     email.value = "";
 
-    password.value = "";
-};
+//     password.value = "";
+// };
 
-loginBtn.addEventListener("click", loginHandler);
+// loginBtn.addEventListener("click", loginHandler);
