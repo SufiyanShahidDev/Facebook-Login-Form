a63a745899bc06f21adec09e1da8be0b11a39b86
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-20 20:44:42 +0500

    Initial commit

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ let loginBtn = document.getElementById("loginBtn");
 
 let userDataArr = [];
 
-let loginHandler = () => {
+// const loginHandler = () => {
     const sweetAlert = (error, title, message) => {
         Swal.fire({
             icon: error,
